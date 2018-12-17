6cbd7b7c311fdb88376cba527a3ffe6e8ef58b62
史恭文 <844910815@qq.com>
2018-12-17 14:16:01 +0800

    美化了下界面并且升级springboot 为2.0

--- src/main/java/com/cn/sleep/study/example_1/ExampleOneController.java
+++ src/main/java/com/cn/sleep/study/example_1/ExampleOneController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -23,5 +24,10 @@ public class ExampleOneController {
                 put("isSuccess", true);
             }
         };
+    }
+
+    @RequestMapping("findAll")
+    public List<Model> findAll() {
+        return repository.findAll();
     }
 }
--- src/main/resources/static/exp_1_app.js
+++ src/main/resources/static/exp_1_app.js
@@ -1,7 +1,25 @@
 $(function () {
+
     $("#submitButton").click(() => {
         $("#studentForm").ajaxSubmit(function (res) {
-            alert(res);
+            loadStudent();
         })
     });
+
+    function loadStudent() {
+        $.post(webctx + "exp_1/findAll", function (data) {
+            let tr = '';
+            if (data) {
+                data.forEach((ele) => {
+                    let tb = `<tr>
+                            <td>${ele['id']}</td>
+                            <td>${ele['name']}</td>
+                            <td>${ele['age']}</td>
+                           </tr>`;
+                    tr += tb;
+                })
+                $('#studentListTable').html(tr);
+            }
+        }, 'json')
+    }
 });
